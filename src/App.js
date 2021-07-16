9aec963f0ad5f99f7053281d6074e71caf853ad8
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2021-07-16 20:49:59 +0300

    stiller ve yapı değiştirildi

--- src/App.js
+++ src/App.js
@@ -2,11 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import JWT from 'jsonwebtoken';
 import faker from 'faker';
-import { Row, Col, Table } from 'antd';
+import { Layout, Row, Col, Table, Tag } from 'antd';
 
 import 'antd/dist/antd.css'
 
 import './App.css';
+
+const columns = [
+  {
+    title: 'Ülke',
+    dataIndex: 'country',
+    key: 'country',
+  },
+  {
+    title: 'İsim',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Sıra',
+    dataIndex: 'rank',
+    key: 'rank',
+  },
+  {
+    title: 'Para',
+    dataIndex: 'money',
+    key: 'money',
+  },
+  {
+    title: 'Günlük Değişim',
+    dataIndex: 'rankDiff',
+    key: 'rankDiff',
+    render: diff => (
+      <span>
+        <Tag color={diff === 0 ? 'yellow' : (diff < 0 ? 'red' : 'green')} key={diff}>
+          {diff}
+        </Tag>
+      </span>
+    )
+  },
+];
 
 let user = {
   id: parseInt(Math.random() * 10000000) % 10000000,
@@ -18,28 +53,6 @@ let socket;
 function App() {
 
   const [score, setScore] = useState([]);
-  const [columns, setColumns] = useState([
-    {
-      title: 'Sıra',
-      dataIndex: 'rank',
-      key: 'rank',
-    },
-    {
-      title: 'İsim',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Para',
-      dataIndex: 'money',
-      key: 'money',
-    },
-    {
-      title: 'Ülke',
-      dataIndex: 'country',
-      key: 'country',
-    },
-  ]);
 
   useEffect(() => {
     const token = JWT.sign(user, 'fakesecret');
@@ -56,6 +69,7 @@ function App() {
       setScore(score.map(x => ({
         ...x,
         rank: x.rank + 1,
+        rankDiff: x.todayRank - x.rank,
       })));
     });
 
@@ -70,25 +84,34 @@ function App() {
   };
 
   return (
-    <Row
-      justify='center'>
-      <Col
-        md={6}>
-        <Table
-          rowKey={x => x.id}
-          rowSelection={{
-            selectedRowKeys: [user.id],
-          }}
-          dataSource={score}
-          columns={columns}
-          pagination={{ defaultPageSize: 106, position: ['none', 'none']}}
-        />
+    <Layout.Content>
+      <Row
+        justify='center'>
+        <Col
+          md={6}>
+          <h3>Skor Tablosu</h3>
+        </Col>
+      </Row>
+      <Row
+        justify='center'>
+        <Col
+          md={6}>
+          <Table
+            rowKey={x => x.id}
+            rowSelection={{
+              selectedRowKeys: [user.id],
+            }}
+            dataSource={score}
+            columns={columns}
+            pagination={{ defaultPageSize: 106, position: ['none', 'none']}}
+          />
 
-        <button className='game-end' onClick={endGame}>
-          Oyundan Para Kazan
-        </button>
-      </Col>
-    </Row>
+          <button className='game-end' onClick={endGame}>
+            Oyundan Para Kazan
+          </button>
+        </Col>
+      </Row>
+    </Layout.Content>
   );
 }
 
